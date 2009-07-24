fb8a7882f3aa23f5722db0a43e21370bbd217893
Giles Bowkett <gilesb@gmail.com>
2009-07-24 14:03:09 -0700

    refactoring

--- tweenr.rb
+++ tweenr.rb
@@ -8,13 +8,21 @@ def frame_id(number)
   "output/tween#{sprintf("%.4d", number)}"
 end
 
-(0..100).each do |number|
-  @cx = number * 9
-  @cy = number * 7
+def render(template)
+  ERB.new(File.read(template)).result(binding)
+end
+
+# this is really another render method
+def graphic(number)
   File.open("#{frame_id(number)}.svg", "w") do |file|
-    file.write ERB.new(File.read(@template)).result(binding)
+    file.write render(@template)
   end
   system("convert #{frame_id(number)}.svg #{frame_id(number)}.jpg")
   File.unlink("#{frame_id(number)}.svg")
 end
 
+(0..100).each do |number|
+  @cx = number * 9
+  @cy = number * 7
+  graphic(number)
+end
